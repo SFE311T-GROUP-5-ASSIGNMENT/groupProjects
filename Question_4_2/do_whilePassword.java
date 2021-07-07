99637b5d4dd081a1a3bde62a8457fd236454b731
Nkstefaans <nkstefaans1@gmail.com>
2021-07-07 20:51:43 +0200

    Syntax Errors Fixed

--- Question_4_2/do_whilePassword.java
+++ Question_4_2/do_whilePassword.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class do_whilePassword {
     public static void main(String[] args) {
         String choice =""; do{
-            //System.out.println("Enter today's number from the menu"); choice = 
-        JOptionPane.showInputDialog("Please enter in a password");
-        }while(choice != "John");}}
+            //System.out.println("Enter today's number from the menu"); there was no indenting choice has been properly indented into line 8  
+        choice = JOptionPane.showInputDialog("Please enter in a password");
+        }while(choice != "John");} //Extra curly brace deleted
 }
