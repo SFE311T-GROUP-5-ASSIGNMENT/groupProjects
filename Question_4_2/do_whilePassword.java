c29bffe8b69d1681e1a03ffeca13671c2e6ece62
Nkstefaans <nkstefaans1@gmail.com>
2021-07-08 17:26:44 +0200

    Code modified to validate the new password entered to check for java identifier rules

--- Question_4_2/do_whilePassword.java
+++ Question_4_2/do_whilePassword.java
@@ -1,11 +1,32 @@
 import java.util.*; 
 import javax.swing.*;
+import java.util.regex.*;
+
 
 public class do_whilePassword {
+
+        public static boolean passChecked(String choice){
+            String regex = "^[A-Za-z]\\w{5,29}$"; //“^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$”
+            //String regex2 = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{1, 20}$";
+    
+            Pattern p = Pattern.compile(regex);
+    
+            if(choice == null){
+                return false;
+            }
+            Matcher m = p.matcher(choice);
+    
+            return m.matches();
+        } 
     public static void main(String[] args) {
         String choice = "";
         String choice1 = "";
+        String choice2 = "";
+        String newPassword = "";
         int count = 0;
+        //PassCheck myObject = new PassCheck();
+    
+
         String password = "John"; 
         do{
             
@@ -15,21 +36,29 @@ public class do_whilePassword {
 
             if (password.equals(choice1) == false && count < 3){
                 System.out.println(choice1 + " is incorrect try again.");
-                //JOptionPane.showInputDialog("Incorrect passord. Please enter password");
-                //choice1 = choice;
+                // choice = JOptionPane.showInputDialog("Incorrect password. Please enter password");
+                // choice1 = choice;
             }
             else if (count == 3){
                 System.out.println("You have entered a wrong password 3 times, your password is " + password);
-                choice = JOptionPane.showInputDialog("You have entered a wrong password 3 times, your password was " + password + ". Please enter a new password");
+                choice2 = JOptionPane.showInputDialog("You have entered a wrong password 3 times, your password was " 
+                                                        + password + ". Please enter a new password");
+                newPassword = choice2;
+                do{
+                    System.out.println(choice2 + " does not meet requirements try again.");
+                    choice2 = JOptionPane.showInputDialog("Password does not meet Requirements please try again");
+                    newPassword = choice2;
+                }while(passChecked(newPassword) == false);
                 break;
             }
-        }while(password.equals(choice1)==false);
+        }while(password.equals(choice1) == false);
 
         if(choice1.equals(password)){
             System.out.println("Access Granted");
         }
+        
         else{
-            System.out.println("Password Changed");
+            System.out.println("Password change was successful");
         }
         
     }
