acea21ccaa14b78e655fa5f86d1d0242513f7134
Nkstefaans <nkstefaans1@gmail.com>
2021-07-09 13:06:57 +0200

    Code modification/correction

--- Question_4_3_2/longestConsecutiveSeq.py
+++ Question_4_3_2/longestConsecutiveSeq.py
@@ -1,8 +1,11 @@
-myList = [100 ,4 ,200 ,1 ,3 ,2,5]
+myList = [100,4,200,1,3,2,5]
 myList.sort()
 consecutives = []
+count = 0
 
 for i in range(len(myList)):
-    if myList[i] == myList[i - 1] + 1:
+    #if myList[i - 1] <= myList[i]:
+    if (myList[i] - myList[i-1]) == 1 or (myList[i] - myList[i-1]) == -1 or myList[i] <= myList[i-1]:
        consecutives.append(myList[i])
+       count = count + 1
 print(consecutives)
