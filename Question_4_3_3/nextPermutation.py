bd047c454648a2860bf5d1f907ead21b4ced2380
Nkstefaans <nkstefaans1@gmail.com>
2021-07-11 17:16:31 +0200

    Solution's first push

---
+++ Question_4_3_3/nextPermutation.py
@@ -0,0 +1,18 @@
+def nextPermutations(myList):
+    listLen = len(myList)
+    index = -1
+    for i in range(listLen - 1):
+        if myList[i] < myList[i + 1]:
+            index = i
+    if index == -1:
+        nextPermutationList = myList.sort()    
+    else:   
+        for j in range(listLen -1, index, -1):
+            if myList[j] > myList[index]:
+                myList[index], myList[j] = myList[j], myList[index]
+                nextPermutationList = myList[:index+1] + list(reversed(myList[index+1:]))
+                break
+    return nextPermutationList
+
+myList = [1,2,3]
+print(nextPermutations(myList))
