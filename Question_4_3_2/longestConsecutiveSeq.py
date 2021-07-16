42ed059c5b3429f5e0efe4ee0560b228ea9bdb15
Nkstefaans <nkstefaans1@gmail.com>
2021-07-16 19:00:46 +0200

    Code rectification

--- Question_4_3_2/longestConsecutiveSeq.py
+++ Question_4_3_2/longestConsecutiveSeq.py
@@ -6,16 +6,16 @@ def longestConsecutive(listOfNums):
     #sorting the list
     listOfNums.sort()
     #initialising of the counter
-    permutation_counter = 0
+    consecutive_counter = 0
     
     #loop created to loop through the list of given values
     for i in range(len(listOfNums)):
         #checks for consecutive values then updates the counter when one is found
         if (listOfNums[i] - listOfNums[i-1]) == 1 or (listOfNums[i] - listOfNums[i-1]) == -1 or listOfNums[i] <= listOfNums[i-1] and listOfNums[i] != listOfNums[i-1]:
             #Updating the counter
-            permutation_counter = permutation_counter + 1
+            consecutive_counter = consecutive_counter + 1
     #returning the number of consecutive valuesfound
-    return permutation_counter
+    return consecutive_counter
 
 if __name__ == '__main__':
     #list to be checked for consecutives
